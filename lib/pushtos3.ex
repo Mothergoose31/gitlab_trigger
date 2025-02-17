24b691bdf01001eddaa7b0e1bcec0f7ee2a0547d
Joseph Alvarenga Beech <JosephAlvarengaBeech@protonmail.com>
2025-02-17 15:23:57 -0800

    updated to be more dynamic

--- lib/pushtos3.ex
+++ lib/pushtos3.ex
@@ -1,7 +1,21 @@
 defmodule PushToS3Bucket do
+  @moduledoc """
+  PushToS3Bucket module for pushing artifacts to S3 bucket.
+
+  This module is used to push artifacts to S3 bucket.
+  it takes in the base url which is the url you want to push to,
+  and the output file which is the file that contains the artifacts to push to S3 bucket.
+
+  at some point will be done in conjuction with xargs something
+  like
+  mix run push_to_s3_bucket.exs -- --base_url --output_file |
+  xargs -I {} ... ect
+  """
+
+  @spec run() :: :ok
   def run do
     base_url = Application.get_env(:gitlab_pipeline, :base_url, System.argv() |> Enum.at(0))
-    output_file = Application.get_env(:gitlab_pipeline, :output_file, "devops_script/output.txt")
+    output_file = Application.get_env(:gitlab_pipeline, :output_file, System.argv() |> Enum.at(1))
 
     if File.exists?(output_file) do
       artifact_urls =
